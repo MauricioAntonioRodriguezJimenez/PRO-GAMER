10eedbb925741131e501c7edb35a9a41c06407e3
mauriciorodriguez4 <98993011+mauriciorodriguez4@users.noreply.github.com>
2022-05-14 12:12:23 -0600

    Update Conectar.java

--- Conectar.java
+++ Conectar.java
@@ -5,23 +5,33 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conectar {
-
-    //Hacer la conexión a la base de datos
-    private static final String URL = "jdbc:mysql://localhost:3306/baseproyecto";
+    private static final String URL  = "jdbc:mysql://localhost:3306/baseproyecto";
     private static final String Driver = "com.mysql.jdbc.Driver";
     private static final String Usuario = "root";
     private static final String Pass = "";
-    private static Connection conex = null;
-
-    public static Connection getConexion() {
-        try {
-            if (conex == null) {
+    private static Connection  conex = null;
+    public static Connection getConexion(){
+        try{
+            if(conex ==null){
                 Class.forName(Driver);
-                conex = DriverManager.getConnection(URL, Usuario, Pass);
+                conex=DriverManager.getConnection(URL, Usuario , Pass);
             }
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Error: " + e);
+        } catch (ClassNotFoundException | SQLException e){
+            System.out.println("Error:  No se logro establecer conexion con la base");
         }
-        return conex;
+    return conex;
+}
+    
+    
+
+
+    //Prueba de Conexion a la base de Datos
+    public static void pruebaConex() {
+        if (Conectar.getConexion() != null) {
+            System.out.println("Conexión a Base de Datos, ha sido correcta");
+        } else {
+            System.out.println("No se ha podido conectar a la base de datos");
+        }
+
     }
 }
