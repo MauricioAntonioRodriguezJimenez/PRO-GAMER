ae76898e42459995738d27bdb3f46f60032a5fa1
mauriciorodriguez4 <98993011+mauriciorodriguez4@users.noreply.github.com>
2022-05-14 12:10:45 -0600

    Update Login.java

--- Login.java
+++ Login.java
@@ -2,81 +2,95 @@ package proyecto.programacion;
 
 import java.util.Scanner;
 
-/**
- *
- * @author MauriRod
- */
 public class Login extends Empleado {
 
-    public Login() {
-        super("Mauricio", "Rodríguez", 20012, 'M', 2001);
-
-    }
+    
 
     public void logearse() {
-        Scanner rm = new Scanner(System.in);
+        Scanner datosEntra = new Scanner(System.in);
 
-        String name, pass, p = "tiendaPRO2022";
-        int id, c = 0;
-
+        String name="", contrasenha="", palabra = "hola";
+        int id, contador = 0;
+        
+        try{
         System.out.println("Ingrese su nombre: ");
-        name = rm.nextLine();
+        name = datosEntra.nextLine();
 
         System.out.println("Favor " + name + " ingrese su id: ");
-        id = rm.nextInt();
+        id = datosEntra.nextInt();
 
-        Scanner r = new Scanner(System.in);
+        Scanner dtoContra = new Scanner(System.in);
         System.out.println("Ingrese contraseña brindada por la tienda: ");
-        pass = r.nextLine();
+        contrasenha = dtoContra.nextLine();
+        
 
-        if (p.equals(pass)) {
+        if (palabra.equals(contrasenha)) {
             System.out.println("");
             System.out.println("Hola " + name + " con numero de identificación " + id);
             System.out.println("     Bienvenido al sistema de la tienda      ");
         } else {
             System.out.println("La contraseña es incorrecta");
-            while (!pass.equals(p) && c <= 2) {
+            while (!contrasenha.equals(palabra) && contador <= 2) {
                 System.out.println("");
-                c++;
+                contador++;
                 System.out.println("Por favor, ingrese de nuevo la contraseña: ");
-                pass = r.nextLine();
+                contrasenha = dtoContra.nextLine();
 
-                if (p.equals(pass)) {
+                if (palabra.equals(contrasenha)) {
                     System.out.println("Hola " + name + " con numero de identificación " + id);
                     System.out.println("     Bienvenido al sistema de la tienda      ");
                     System.out.println("");
-                    
+
                     break;
                 }
-                if (c == 2) {
+                if (contador == 2) {
                     System.out.println("Su sesión se ha bloqueado, intente en otro momento.");
                     break;
                 }
 
             }
         }
+      
+    }catch(Exception e){
+            System.out.println("El valor que introduciste no es valido");
+            System.exit(0);
+         }
     }
 
     public void registrarse() {
-        Scanner mr = new Scanner(System.in);
-        String nombre, apellido, edad, tel;
+        Scanner dtoRegistro = new Scanner(System.in);
+        String nombre="", apellido="", edad="", tel="";
+        
+        try{
+            System.out.println("Ingrese su nombre: ");
+            nombre = dtoRegistro.nextLine();
+            System.out.println("Ingrese su apellido: ");
+            apellido = dtoRegistro.nextLine();
+            System.out.println("Digite su edad: ");
+            edad = dtoRegistro.nextLine();
+            System.out.println("Digite su número de teléfono: ");
+            tel = dtoRegistro.nextLine();
+            System.out.println(""); 
+        }catch(Exception e){
+            System.out.println("El valor que se introdujo no es valido...");
+            System.exit(0);
+        }
 
-        System.out.println("Ingrese su nombre: ");
-        nombre = mr.nextLine();
-        System.out.println("Ingrese su apellido: ");
-        apellido = mr.nextLine();
-        System.out.println("Digite su edad: ");
-        edad = mr.nextLine();
-        System.out.println("Digite su número de teléfono: ");
-        tel = mr.nextLine();
-        System.out.println("");
 
+        try{
         System.out.println("Su correo nuevo es: " + nombre.substring(0, 5).toLowerCase() + apellido.substring(0, 3).toLowerCase() + edad.charAt(0) + tel.substring(1, 3) + "@tiendaprogamer.com");
         System.out.println("Su nombre de usuario: " + nombre.substring(0, 5).toLowerCase() + apellido.substring(0, 3).toLowerCase());
         System.out.println("");
         System.out.println("----------------------------------------------------------------------");
-        System.out.println("¡Bienvenido " + nombre+ ", esperamos disfrutes de nuestros productos!" );
+        System.out.println("¡Bienvenido " + nombre + ", esperamos disfrutes de nuestros productos!");
         System.out.println("A continuacion se te mostrará el menú");
+
+        }catch(Exception e){
+            System.out.println("Los campos de entrada son :\n Nombre al menos 5 Caracteres\n Apellido al menos 3 caracteres\n Telefono 6 caracteres\n"
+                    + "Si no cumplen complete el formato con numeros o caracteres especiales");
+            //Para que el programa se cierre.
+           System.exit(0);
+        }
 
     }
 }
